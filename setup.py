441fb07b54b9f5ff47c0772f15913d406a3f751b
hvbargen <hvbargen@c6af38b0-91b1-984a-8d68-447b65cf5a62>
2008-10-26 20:52:57 +0000

    Preparation of release 0.3.0
    git-svn-id: https://svn.code.sf.net/p/deco-cow/code/trunk@52 c6af38b0-91b1-984a-8d68-447b65cf5a62

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 
 setup(
     name = "wordaxe",
-    version = "0.2.6",
+    version = "0.3.0",
     description = "Provide hyphenation for python programs and ReportLab paragraphs.",
     long_description = "Provide hyphenation for python programs and ReportLab paragraphs.",
     author = "Henning von Bargen",
