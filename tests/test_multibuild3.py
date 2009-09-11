5353642de5c4a8ea405881ff7d168b68bca1f6f7
cito <cito@c6af38b0-91b1-984a-8d68-447b65cf5a62>
2009-09-11 19:34:52 +0000

    Added another test for multibuilds that are generated by rst2pdf.
    git-svn-id: https://svn.code.sf.net/p/deco-cow/code/trunk@95 c6af38b0-91b1-984a-8d68-447b65cf5a62

---
+++ tests/test_multibuild3.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+
+"""Test for compatibility with multibuilds made by rst2pdf."""
+
+import unittest
+from StringIO import StringIO
+
+from tests.utils import makeSuiteForClasses, printLocation
+
+try:
+    from rst2pdf.createpdf import RstToPdf
+except ImportError:
+    RstToPdf = None
+
+text = """
+Rst2Pdf MultiBuild Test Case For Wordaxe
+========================================
+
+Blah. Blah.
+
+Blah. Blah. Blah.
+
+Blah. Blah. Blah. Blah. Blah. Blah. Blah. Blah. Blah.
+
+Blah. Blah. Blah. Blah. Blah. Blah. Blah. Blah. Blah.
+
+Blah.
+
+  - Blah.
+  - Blah.
+  - Blah.
+
+Blah. Blah. Blah. Blah. Blah. Blah. Blah. Blah.
+
+  - Blah.
+  - Blah.
+  - Blah.
+
+Blah. Blah. Blah.
+
+Blah. Blah. Blah. Blah. Blah. Blah. Blah. Blah. Blah. Blah. Blah. Blah.
+
+Blah. Blah. Blah.
+
+.. contents::
+
+Test Section
+------------
+
+Blah. Blah. Blah.
+
+Blah. Blah. Blah. Blah. Blah.
+
+Blah. Blah. Blah.
+"""
+
+blah = 'The quick brown fox jumps over the lazy dog'
+
+text = text.replace('Blah', blah)
+
+
+class MultiBuildTestCase(unittest.TestCase):
+    """Test rst2pdf muiltibuild"""
+
+    def test_rst2pdf(self):
+
+        assert RstToPdf, 'rst2pdf not installed - cannot test compatibility.'
+
+        rst2pdf = RstToPdf(breaklevel=0)
+
+        rst2pdf.styles['heading1'].spaceBefore = 36
+
+        buffer = StringIO()
+
+        rst2pdf.createPdf(text, output=buffer)
+
+        pdf = buffer.getvalue()
+        buffer.close()
+
+        assert 'ReportLab generated PDF document' in pdf
+        assert 'Rst2Pdf MultiBuild Test Case For Wordaxe' in pdf
+        assert 'Test Section' in pdf
+        assert blah in pdf
+
+
+def makeSuite():
+    return makeSuiteForClasses(MultiBuildTestCase)
+
+if __name__ == "__main__":
+    unittest.TextTestRunner().run(makeSuite())
+    printLocation()
